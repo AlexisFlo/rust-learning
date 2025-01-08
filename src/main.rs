d8458b5273e3792599c3574a585135b81448d2dd
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-08 07:41:25 -0600

    constants and varibales

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
+const MAYORIA_DE_EDAD: u8 = 18; // Las constantes necesitan ser definidas
+
 fn main() {
-    let mi_numero_favorito = 5;
-    let mi_mejor_amigo = "Aragorn";
-
-    println!("Mi número favorito es: {} y mi mejor amigo es: {}", mi_numero_favorito, mi_mejor_amigo); // Rust 2018
-
-    println!("Mi número favorito es {mi_numero_favorito} y mi mejor amigo es {mi_mejor_amigo}"); // Rust 2021 -> Actualmente
+    let mi_variable = 5;
 }
+
 
