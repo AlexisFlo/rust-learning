1ee2760f614b40e923e2874460382d04aa1d5c64
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-08 07:32:50 -0600

    two forms to print data in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mi_numero_favorito = 5;
+    let mi_mejor_amigo = "Aragorn";
+
+    println!("Mi número favorito es: {} y mi mejor amigo es: {}", mi_numero_favorito, mi_mejor_amigo); // Rust 2018
+
+    println!("Mi número favorito es {mi_numero_favorito} y mi mejor amigo es {mi_mejor_amigo}"); // Rust 2021 -> Actualmente
 }
+
