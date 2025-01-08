3c917a23d4e30abba293c523cb5fed5de55d8a21
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-08 07:43:32 -0600

    constants

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 const MAYORIA_DE_EDAD: u8 = 18; // Las constantes necesitan ser definidas
 
 fn main() {
-    let mi_variable = 5;
+    println!("En este país la mayoría de edad es {MAYORIA_DE_EDAD}");
 }
+
 
 
