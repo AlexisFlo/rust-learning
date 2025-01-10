6258068ce74e3512ecd4386f4fe404edb970db07
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-10 07:24:46 -0600

    using char

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
 fn main() {
-    // True
-    // False
-
-    // clean code
-    // Es -> Is/Are
-    // Tiene -> Has/Have
-    let is_raining = true;
-    let has_videogames = true;
+    let mi_letra: char = 'A';
     
-    if is_raining && has_videogames { // Solo entra si la condición es verdadera
-        println!("La persona se queda jugando videojuegos");
-    } else { // En el else entra solo si la condición anterior no se cumple
-        println!("La persona salio a la calle");
-    }
-}
 
+    println!("La inicial de mi nombre es {mi_letra}");
+}
