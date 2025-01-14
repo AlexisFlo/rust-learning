87272dad4925c5513bfd11381131f006a5d5cd99
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-13 21:55:28 -0600

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
 
-    let mi_tupla = ("Alexis", 24, true);
+    let mut mi_vec = vec![1,2,3,4,5];
+    let mut lista_de_nombres = vec!["Alexis", "Arturo", "Alan"];
 
-    println!("Mi nombre es: {}", mi_tupla.0);
+    println!("Mis valores: {mi_vec:?}");
+    println!("Lista de nombres: {lista_de_nombres:?}");
+
+    mi_vec.push(7);
+    lista_de_nombres.push("Anayeli");
+
+    println!("Mis valores: {mi_vec:?}");
+    println!("Lista de nombres: {lista_de_nombres:?}");
 }
 
