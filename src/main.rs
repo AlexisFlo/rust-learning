a70002981eea163677c10525d4216d43fac78c66
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-16 06:56:12 -0600

    simplify match

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
     // Switch
     
-    match 1 {
-         1..=4 => println!("Entra en el caso nro 1 al 4"),
-         _ => todo!()
+
+    let mi_tupla = ("hola", 10);
+    match mi_tupla {
+        (_,1..=4) => println!("Entra en el caso nro 1 al 4 sin importar el texto"),
+        ("hola", 5..=12) => println!("Esta entre el 5 y el 12 y dice hola"), 
+        ("hola", _) => println!("Nos interesa verificar si dice hola, no nos importa el numero"), 
+        _ => todo!()
     }
     
 }
