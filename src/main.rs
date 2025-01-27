87b48b167a0d6145dc2a23e032e56ccf1616caed
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-26 20:49:43 -0600

    doing some examples of shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("El valor de x en el contexto interno es: {x}"); 
-    }
-    println!("El valor de x en el contexto externo es: {x}");
+    // Refinamiento de Tipos
+    let spaces = "  ";
+    let spaces = spaces.len();
+    println!("Longitud de la cadena: {spaces}");
+
+    // Transformaciones intermedias
+    let price = 100;
+    let price = price * 2;
+    let price = price - 10;
+    println!("The price is: {price}");
+
+    // Evitar mutabilidad
+    // let mut count = 1;
+    // count += 1;
+    // se puede escribir como ->
+    let count = 1;
+    let count = count + 1;
+    println!("The count is in: {count}");
 }
-
-
+// doing a web server 
+ 
