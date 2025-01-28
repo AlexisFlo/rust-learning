35dd17c52d6f79b1a3480b86a0a5980edcd72865
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-28 12:57:27 -0600

    Display and add complex structure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,69 @@
+use std::fmt; // Import `fmt`
+
+// A structure holding two numbers. `Debug` will be derived so the results can
+// be contrasted with `Display`.
 #[derive(Debug)]
-struct Person<'a> {
-    name: &'a str,
-    age: u8
+struct MinMax(i64, i64);
+
+// Implement `Display` for `MinMax`
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+// Define a structure where the fields are nameable for comparision
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// Similarly, implement `Display` for `Point2D`
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only 'x' and 'y' are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
 }
 
 fn main() {
-    let name = "Peter";
-    let age = 27;
-    let peter = Person { name, age };
+   let minmax = MinMax(0, 14);
 
-    // Pretty print
-    println!("{:#?}", peter);
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range);
+
+    let point = Point2D {x: 3.3, y: 7.2};
+
+    println!("Compare ponts:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let complex = Complex{real: 3.3, imag: 7.2};
+
+    println!("Compare ponts:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
+
 }
