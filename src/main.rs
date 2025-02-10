77a7b198a88c2db7ce442128dc4add4de49e1230
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-02-10 13:09:37 -0600

    Activity 1. add display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Formatter, Display};
+
 fn reverse(pair: (i32, bool)) -> (bool, i32) {
     // 'let' can be used to bind the members of a tuple to variables.
     let (int_param, bool_param) = pair;
@@ -5,9 +7,20 @@ fn reverse(pair: (i32, bool)) -> (bool, i32) {
     (bool_param, int_param)
 }
 
+fn transpose(matrix: Matrix) -> Matrix {
+    
+    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+}
+
 // The following struct is for the activity
 #[derive(Debug)]
 struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
 
 fn main() {
     // A tuple with a bunch of different types.
@@ -48,5 +61,6 @@ fn main() {
     println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
 
     let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{}", matrix);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
 }
